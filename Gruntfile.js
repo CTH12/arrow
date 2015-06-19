416b715761100146276a6c77c4049072aaa7439e
luis rosario <lrosario101@gmail.com>
2015-06-18 23:51:46 -0400

    correct export to demo path

--- Gruntfile.js
+++ Gruntfile.js
@@ -44,7 +44,7 @@ module.exports = function(grunt) {
         },
 
         deploy: {
-            folder:                 '../../../../../demo',
+            folder:                 '../../../../demo',
         }
     };
 
